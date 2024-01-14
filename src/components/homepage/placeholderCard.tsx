406faf0baa58ad6dd8755d8f5e1ea08392f3eb4c
grigorijtomczuk <grigorijtomczuk@gmail.com>
2024-01-14 20:53:01 +0300

    Alter new placeholder card title; change homepage socials spacing

--- src/components/homepage/placeholderCard.tsx
+++ src/components/homepage/placeholderCard.tsx
@@ -6,7 +6,7 @@ import { faCloudDownload } from "@fortawesome/free-solid-svg-icons";
 const PlaceholderCard = () => {
 	return (
 		<div className="placeholder-card">
-			<Card icon={faCloudDownload} title="Здесь могла быть Ваша реклама" body={<div className="placeholder-card-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus enim tempore debitis numquam, blanditiis unde natus ea delectus nobis harum provident. Voluptatum distinctio fugiat odio animi! Soluta temporibus nesciunt ratione harum rem!</div>} />
+			<Card icon={faCloudDownload} title="Загрузки" body={<div className="placeholder-card-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus enim tempore debitis numquam, blanditiis unde natus ea delectus nobis harum provident. Voluptatum distinctio fugiat odio animi! Soluta temporibus nesciunt ratione harum rem!</div>} />
 		</div>
 	);
 };
