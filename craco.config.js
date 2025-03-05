e4c822fedf63c151d6b52eaae37f9f1d011de66e
Grigory Tomchuk <grigorijtomczuk@gmail.com>
2025-03-06 01:49:31 +0300

    Disable css minifier; replace css transitions with var

--- craco.config.js
+++ craco.config.js
@@ -2,6 +2,9 @@ const path = require("path");
 
 module.exports = {
 	webpack: {
+		plugins: {
+			remove: ["CssMinimizerPlugin"],
+		},
 		alias: {
 			"@": path.resolve("src"),
 			types: path.resolve("types"),
