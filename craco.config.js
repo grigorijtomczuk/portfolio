4b4563345e46e617655cf58b061a06676547c4cf
Grigory Tomchuk <grigorijtomczuk@gmail.com>
2025-03-06 17:27:26 +0300

    Clean up; add postcss-nesting

--- craco.config.js
+++ craco.config.js
@@ -2,12 +2,14 @@ const path = require("path");
 
 module.exports = {
 	webpack: {
-		// plugins: {
-		// 	remove: ["CssMinimizerPlugin"],
-		// },
 		alias: {
 			"@": path.resolve("src"),
 			types: path.resolve("types"),
+		},
+	},
+	style: {
+		postcss: {
+			plugins: [require("postcss-nesting")],
 		},
 	},
 };
